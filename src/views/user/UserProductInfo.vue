124fab4df8ce171024b4d9b20e36eef8dfe49c30
Ann Chou <yuanchou.ann@gmail.com>
2024-04-14 13:37:34 +0800

    router index.js 僅保留產品列表

--- src/views/user/UserProductInfo.vue
+++ src/views/user/UserProductInfo.vue
@@ -96,7 +96,7 @@
         </button>
       </div>
       <div>
-        <p>router index.js 僅保留產品</p>
+        <p>router index.js 僅保留產品列表</p>
         <section class="bg-primary text-white py-md-8 py-7">
           <div class="accordion accordion-flush" id="accordionFlushExample">
             <div class="accordion-item">
