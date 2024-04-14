c29899b6e88ee028d096a1198115b2828793bc72
Ann Chou <yuanchou.ann@gmail.com>
2024-04-14 12:29:42 +0800

    test

--- src/views/user/UserProductInfo.vue
+++ src/views/user/UserProductInfo.vue
@@ -25,7 +25,7 @@
         </div>
       </div>
       <!-- 介紹 -->
-      <div class="col-12 col-md-4">
+      <!-- <div class="col-12 col-md-4">
         <div class="p-3 p-lg-7 mb-3 mb-md-6 bg-tertiary">
           <div class="d-flex justify-content-between align-items-start mb-5 mb-md-7">
             <div>
@@ -94,59 +94,54 @@
           <span v-else>已絕版．NT$ {{
             product.productInfo.price.toLocaleString() }}</span>
         </button>
-      </div>
+      </div> -->
       <div>
         <section class="bg-primary text-white py-md-8 py-7">
-    <div class="container">
-        <div class="row gy-5">
-            <div class="col-md-4">
-                <div class="h-100 d-flex align-items-center">
-                    <h3 class="mb-0"><b>常見問答</b></h3>
-                </div>
+          <div class="accordion accordion-flush" id="accordionFlushExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="flush-headingOne">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                  Accordion Item #1
+                </button>
+              </h2>
+              <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
+                data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the
+                  <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+              </div>
             </div>
-            <div class="col-md-8">
-                <div class="accordion mb-5" id="faq">
-                  <div class="accordion-item">
-                        <div class="accordion-header">
-                            <button
-                                type="button" class="bg-light accordion-button collapsed"
-                                data-bs-toggle="collapse" :data-bs-target="`#faq1`"
-                                aria-expanded="false" :aria-controls="`#faq-1`">
-                                <i class="d-none d-md-block
-                                fs-7 text-highlight bi bi-asterisk me-3"></i>
-                                <b>ques</b>
-                            </button>
-                        </div>
-                        <div
-                            class="accordion-collapse collapse" :id="`faq1`" aria-expanded="false">
-                            <div class="accordion-body">
-                                <p class="mb-0 lh-lg">123123123</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="accordion-item">
-                        <div class="accordion-header">
-                            <button
-                                type="button" class="bg-light accordion-button collapsed"
-                                data-bs-toggle="collapse" :data-bs-target="`#faq2`"
-                                aria-expanded="false" :aria-controls="`#faq-2`">
-                                <i class="d-none d-md-block
-                                fs-7 text-highlight bi bi-asterisk me-3"></i>
-                                <b>ques</b>
-                            </button>
-                        </div>
-                        <div
-                            class="accordion-collapse collapse" :id="`faq2`" aria-expanded="false">
-                            <div class="accordion-body">
-                                <p class="mb-0 lh-lg">123123123</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="flush-headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                  Accordion Item #2
+                </button>
+              </h2>
+              <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo"
+                data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the
+                  <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this
+                  being filled with some actual content.</div>
+              </div>
             </div>
-        </div>
-    </div>
-</section>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="flush-headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                  Accordion Item #3
+                </button>
+              </h2>
+              <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree"
+                data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the
+                  <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting
+                  happening here in terms of content, but just filling up the space to make it look, at least at first
+                  glance, a bit more representative of how this would look in a real-world application.</div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   </div>
